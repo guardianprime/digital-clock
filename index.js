acb9b6b7010575fd095f7c963e520aea58269df9
Guardianprime <guardianacer@outlook.com>
2024-02-08 11:26:45 +0100

    first stable state

---
+++ index.js
@@ -0,0 +1,25 @@
+const hourEl = document.getElementById("hour")
+const minutesEl = document.getElementById("minutes")
+const secondsEl = document.getElementById("seconds")
+const ampmEl = document.getElementById("ampm")
+
+function updateTime() {
+    let hr = new Date().getHours()
+    let min = new Date().getMinutes()
+    let secs = new Date().getSeconds()
+    if (hr > 12) {
+        hr -= 12
+        ampmEl = "PM"
+    }
+
+    hr < 10 ? hr = "0" + hr : hr = hr
+    min < 10 ? min = "0" + min : min = min
+    secs < 10 ? secs = "0" + secs : secs = secs
+
+    hourEl.innerText = hr
+    minutesEl.innerText = min
+    secondsEl.innerText = secs
+    setTimeout(() => { updateTime() })
+}
+
+updateTime()
