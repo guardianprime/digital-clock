4683e52e9f4172085d9e26139d6611766b0f5b2d
Guardianprime <guardianacer@outlook.com>
2024-02-10 00:25:00 +0100

    last state

--- index.js
+++ index.js
@@ -4,23 +4,24 @@ const secondsEl = document.getElementById("seconds")
 const ampmEl = document.getElementById("ampm")
 
 function updateTime() {
+
     let hr = new Date().getHours()
     let min = new Date().getMinutes()
     let secs = new Date().getSeconds()
     if (hr > 12) {
         hr -= 12
-        ampmEl = "PM"
+        ampmEl = "pm"
     }
 
-    hr < 10 ? hr = "0" + hr : hr = hr
-    min < 10 ? min = "0" + min : min = min
-    secs < 10 ? secs = "0" + secs : secs = secs
-
+    hr < 10 ? hr = "0" + hr : hr
+    secs < 10 ? secs = "0" + secs : secs
+    min < 10 ? min = "0" + min : min
     hourEl.innerText = hr
     minutesEl.innerText = min
     secondsEl.innerText = secs
     ampmEl, (innerText = ampm)
-    setTimeout(() => { updateTime() }, 1000)
+    setTimeout(() => {
+        updateTime()
+    }, 1000)
 }
-
 updateTime()
